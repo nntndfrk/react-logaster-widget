a8fbff05d555a65b81de7348d8b6a1bf3bc7e449
Michail Smolinskiy <nntndfrk@gmail.com>
2020-12-05 12:36:03 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <lg-widget dev="1" lang="en" type="3">
+        <div className="container">
+          <h4>Lorem ipsum dolor sit amet, consectetur.</h4>
+          <div>
+            <input type="text" data-target="create-input" />
+            <button type="button" data-target="create-button">
+              Create
+            </button>
+          </div>
+        </div>
+      </lg-widget>
     </div>
   );
 }
